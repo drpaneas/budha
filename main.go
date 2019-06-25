4e1b401049fa81f3b7f5432f0e776a82f4aa5171
Panagiotis Georgiadis <pgeorgiadis@suse.com>
2019-06-25 17:48:20 +0200

    Third

--- main.go
+++ main.go
@@ -2,86 +2,81 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/yosssi/gohtml"
 )
 
-func getTitleHTML(data string) (pageTitle string) {
-	// Find a substring
-	titleStartIndex := strings.Index(data, "<title>")
-	if titleStartIndex == -1 {
-		fmt.Println("No title element found")
-		os.Exit(0)
-	}
-
-	// The start index of the title is the index of the first character, the < symbol.
-	// We don't want to include <title> as part of the final value, so let's offset the index
-	// by the number of characters in <title> that is 7 chararacters
-	titleStartIndex += 7
-
-	// Find the index of the closing tag
-	titleEndIndex := strings.Index(data, "</title>")
-	if titleEndIndex == -1 {
-		fmt.Println("No closing tag for title found.")
-		os.Exit(0)
-	}
-
-	// Copy the substring into a separate variable
-	// so the variables with the full document data can be garbage collected
-	pageTitle = string([]byte(data[titleStartIndex:titleEndIndex]))
-	return pageTitle
+type Movie struct {
+	title       string // e.g. Venom 4K UHD
+	link        string // e.g. https://blu-ray-rezensionen.net/venom-4k-uhd/
+	real4K      string // e.g. Ja (4K DI)
+	director    string // e.g. James Ivory
+	studio      string // e.g. Disney
+	audioFormat string // e.g. Dolby Atmos
+	codec       string // e.g. HEVC
+	hdr         string // e.g. HDR10
+	length      string // e.g. 130
+	ratio       string // e.g. 2,35:1
+	country     string // e.g. USA
+	year        string // e.g. 2017
+	actors      string // e.g. Chris Hemsworth, Tom Hiddleston, Cate Blanchett
+	pq          string // 85%
 }
 
-func getLinks(website string, anchor string) (string, error) {
+var Collection = []Movie{
+	title:       movieTitle,
+	link:        movieLink,
+	real4K:      movieReal4K,
+	director:    movieDirector,
+	studio:      movieStudio,
+	audioFormat: movieAudioFormat,
+	codec:       movieCodec,
+	hdr:         movieHDR,
+	length:      movieLength,
+	ratio:       movieRatio,
+	country:     movieCountry,
+	year:        movieYear,
+	actors:      movieActors,
+	pq:          moviePQ,
+})
+}
 
-	// Create a goquery document from the HTTP response
-	doc, err := goquery.NewDocument(website)
+func titleAndURL(website string, Collection []Movie) *goquery.Document {
+	// Request the HTML page.
+	res, err := http.Get(website)
 	if err != nil {
-		log.Fatal("Error loading HTTP response body. ", err)
-		os.Exit(1)
+		log.Fatalf("[error] [http.Get()]: %s", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("[error] [res.StatusCode] code:%d status:%s", res.StatusCode, res.Status)
 	}
 
-	fmt.Println(doc)
-
-	// Find all links and process them
-	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
-		href, _ := item.Attr("href")
-		// fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
-		if strings.Contains(string(item.Text()), anchor) {
-			fmt.Printf("link: %s - anchor text: %s\n", href, item.Text())
-			return href
-		}
-	})
-	return "", nil
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal("Cannot load the HTML document: %s", err)
+	}
+	return doc
 }
 
 func main() {
-	// Call http.Get with the URL we want to retrieve
-	res, err := http.Get("https://blu-ray-rezensionen.net/ultra-hd-blu-ray/")
-	if err != nil {
-		log.Fatal(err)
+	doc := titleAndURL("https://blu-ray-rezensionen.net/ultra-hd-blu-ray/", Collection)
+	doc.Find("a[href]").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		href, _ := s.Attr("href")
+		if strings.Contains(string(s.Text()), "UHD") {
+			movieTitle := s.Text()
+			movieLink := href
+		}
+		return true
+	})
+
+	for _, Collection := range Collection {
+		fmt.Println("Movie:", Collection.title)
+		fmt.Println("Link:", Collection.link)
 	}
-	// Release the network connection once the "main" function exists
-	defer res.Body.Close()
-
-	// Read all the data in the response
-	dataInBytes, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	dataInString := string(dataInBytes)
-	dataInFormattedString := gohtml.Format(dataInString)
-
-	// Convert the data to a string and print it
-	fmt.Println(dataInFormattedString)
-
-	// Print out the result
-	fmt.Println(getTitleHTML(dataInFormattedString))
 
 }
